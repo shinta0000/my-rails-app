2f14d1c187501e177c5c5428ce73b7d5f91b941c
ueno <uenoshinta0k@gmail.com>
2020-11-03 15:37:35 +0900

    users controllerを作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
+  get 'users/new'
   root 'simplepages#home'
   get 'simple_pages/home'
   get 'simple_pages/help'
   get 'simple_pages/about'
+  get 'users/new'
 end
--- test/controllers/simple_pages_controller_test.rb
+++ test/controllers/simple_pages_controller_test.rb
@@ -2,11 +2,6 @@ require 'test_helper'
 
 class SimplePagesControllerTest < ActionDispatch::IntegrationTest
 
-  test "should get root" do
-    get simple_pages_home_url
-    assert_response :success
-  end
-
   test "should get home" do
     get simple_pages_home_url
     assert_response :success
