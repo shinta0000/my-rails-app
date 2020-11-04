cb0280390cbd9adf43c631564fea5e7de9cefd8b
ueno <uenoshinta0k@gmail.com>
2020-11-04 20:30:54 +0900

    [add] Userモデルを追加

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,6 @@ Rails.application.routes.draw do
   get '/help', to: 'simple_pages#help'
   get '/about', to: 'simple_pages#about'
   get '/home', to: 'simple_pages#home'
-  get '/users/new', to: 'users#new'
-  get '/users/login', to: 'users#login'
+  get '/new', to: 'users#new'
+  get '/login', to: 'users#login'
 end
